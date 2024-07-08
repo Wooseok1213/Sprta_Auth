498d210fa7b53c9a271c938e99d81fe07dd06259
Wooseok1213 <162704734+Wooseok1213@users.noreply.github.com>
2024-07-08 19:51:28 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -2,31 +2,51 @@ package com.sparta.springauth.controller;
 
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequiredArgsConstructor
+
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
-        public final UserService userService;
+    private final UserService userService;
 
-        @GetMapping("/user/login-page")
-        public String loginPage () {
-            return "login";
-        }
-
-        @GetMapping("/user/signup")
-        public String signupPage () {
-            return "signup";
-        }
-
-        @PostMapping("/user/signup")
-        public String signup (SignupRequestDto requestDto){
-            userService.signup(requestDto);
-            return "redirect:/api/user/login-page";
-        }
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+}
